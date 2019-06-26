b4f7c3179dfc104b7ce0014d91bfcd8076bcf20b
Olivia Lee <olivia.suji.lee@berkeley.edu>
2019-06-25 23:04:09 -0700

    Updates

---
+++ precache-manifest.b53c3805ce953e2dcb427d5f79809ff7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e558cdf8de7b90a1ea3a0b521883d7",
+    "url": "/website-redesign/index.html"
+  },
+  {
+    "revision": "e4c85564619330c576ca",
+    "url": "/website-redesign/static/css/main.02e3e0f8.chunk.css"
+  },
+  {
+    "revision": "8e6ea9647247dd441db0",
+    "url": "/website-redesign/static/js/2.4886af56.chunk.js"
+  },
+  {
+    "revision": "e4c85564619330c576ca",
+    "url": "/website-redesign/static/js/main.fa6698cd.chunk.js"
+  },
+  {
+    "revision": "03328ecfa658afb3187b",
+    "url": "/website-redesign/static/js/runtime~main.0ee9bbf1.js"
+  },
+  {
+    "revision": "b20a359689b2b25fc40a5d9459883d28",
+    "url": "/website-redesign/static/media/downarrow.b20a3596.svg"
+  },
+  {
+    "revision": "f1269b0bf1d36606cee0a3e6e8f4d47a",
+    "url": "/website-redesign/static/media/email.f1269b0b.svg"
+  },
+  {
+    "revision": "e94196796fcd701aa3076f765520642a",
+    "url": "/website-redesign/static/media/footer_email.e9419679.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/website-redesign/static/media/footer_github.76d344f0.svg"
+  },
+  {
+    "revision": "8220d48085242e88ff8450680fdd8f7e",
+    "url": "/website-redesign/static/media/footer_linkedin.8220d480.svg"
+  },
+  {
+    "revision": "e4f2d6b028d93b8c7988d2132bf13ca2",
+    "url": "/website-redesign/static/media/github.e4f2d6b0.svg"
+  },
+  {
+    "revision": "20177402a1e05f0de7322149631cefeb",
+    "url": "/website-redesign/static/media/linkedin.20177402.svg"
+  },
+  {
+    "revision": "1571ae25d31fe13af5d35670a0294e3c",
+    "url": "/website-redesign/static/media/olivia.1571ae25.jpg"
+  },
+  {
+    "revision": "7d7b22ee63b0485a61b9af1ff8d57593",
+    "url": "/website-redesign/static/media/pdf.7d7b22ee.svg"
+  }
+]);
